160a127abf230057b6e736fa74e68987e288a48e
Izabela-Pak <iza2002r@gmail.com>
2025-05-20 13:10:46 +0200

    zmiana

--- Blog/Client/src/app/app.component.ts
+++ Blog/Client/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { LewyBanerComponent } from "./lewy-baner/lewy-baner.component";
 import { SrodkowyBanerComponent } from "./srodkowy-baner/srodkowy-baner.component";
 import { PrawyBanerComponent } from "./prawy-baner/prawy-baner.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LewyBanerComponent, SrodkowyBanerComponent, PrawyBanerComponent],
+  imports: [LewyBanerComponent, SrodkowyBanerComponent, PrawyBanerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
