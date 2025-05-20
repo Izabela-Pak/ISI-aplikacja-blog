3722a703ab74e8e9c72ea76c9ac7209483f6fcea
Izabela-Pak <iza2002r@gmail.com>
2025-05-20 11:03:07 +0200

    Update appExpress.js

--- Blog/Server/appExpress.js
+++ Blog/Server/appExpress.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4004;
 
 app.use(express.json());
 const bodyParser = require('body-parser');
